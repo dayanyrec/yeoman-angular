f95abe9e3c6ca9fcc9c0ea0140e742f214e1eb34
Dayany Espindola <dayanyrec@gmail.com>
2018-03-22 10:13:10 -0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanAngularApp
+ * @description
+ * # yeomanAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
